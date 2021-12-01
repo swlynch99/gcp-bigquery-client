aa1b6d060ed5412252d59c18af34a57d07f560fa
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2021-12-01 15:33:18 -0500

    Fix infinite recursion within QueryParameterType

--- src/model/query_parameter_type.rs
+++ src/model/query_parameter_type.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Default, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct QueryParameterType {
-    pub array_type: Box<QueryParameterType>,
+    pub array_type: Option<Box<QueryParameterType>>,
     /// [Optional] The types of the fields of this struct, in order, if this is a struct.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub struct_types: Option<Vec<QueryParameterTypeStructTypes>>,
